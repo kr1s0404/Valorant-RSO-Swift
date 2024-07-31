90c6dc1ff17e7f57d87f6524463a3a3c1f718b40
kr1s0404 <tylerlolz0404@gmail.com>
2024-07-31 23:05:31 +0800

    Add accountManager

--- ValorantRSOSwift/AccountManager.swift
+++ ValorantRSOSwift/AccountManager.swift
@@ -5,4 +5,57 @@
 //  Created by Kris on 7/26/24.
 //
 
-import Foundation
+import SwiftUI
+
+final class AccountManager: ObservableObject
+{
+    @Published var accounts: [Account] = []
+    @Published var currentAccount: Account?
+    
+    private let accountsKey = "savedAccounts"
+    
+    init() {
+        loadAccounts()
+    }
+    
+    func loadAccounts() {
+        if let data = UserDefaults.standard.data(forKey: accountsKey),
+           let decodedAccounts = try? JSONDecoder().decode([Account].self, from: data) {
+            accounts = decodedAccounts
+            currentAccount = accounts.first
+        }
+    }
+    
+    func saveAccounts() {
+        if let encodedData = try? JSONEncoder().encode(accounts) {
+            UserDefaults.standard.set(encodedData, forKey: accountsKey)
+        }
+    }
+    
+    func addOrUpdateAccount(_ account: Account) {
+        if let index = accounts.firstIndex(where: { $0.username == account.username }) {
+            accounts[index] = account
+            if currentAccount?.username == account.username {
+                currentAccount = account
+            }
+        } else {
+            accounts.append(account)
+            if currentAccount == nil {
+                currentAccount = account
+            }
+        }
+        saveAccounts()
+    }
+    
+    func switchToAccount(_ account: Account) {
+        currentAccount = account
+    }
+    
+    func removeAccount(_ account: Account) {
+        accounts.removeAll { $0.username == account.username }
+        if currentAccount?.id == account.id {
+            currentAccount = accounts.first
+        }
+        saveAccounts()
+    }
+}
